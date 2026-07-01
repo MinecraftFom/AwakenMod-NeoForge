aaa9d5c8269dd43952ee1bb57985174a5767829b
MinecraftFom <MinecraftFom477@gmail.com>
2026-07-02 00:49:09 +0800

    Mod main class

---
+++ src/main/java/com/fomdev/awaken/init/Awaken.java
@@ -0,0 +1,14 @@
+package com.fomdev.awaken.init;
+
+import net.neoforged.fml.common.Mod;
+
+@Mod(Awaken.MODID)
+public class Awaken
+{
+    public static final String MODID = "awaken";
+
+    public Awaken()
+    {
+
+    }
+}

cdf2b114a52f6d927e619d06176af34c336fd07e
MinecraftFom <MinecraftFom477@gmail.com>
2026-07-02 23:59:18 +0800

    Finalized getter methods and added a serializable instance

--- src/main/java/com/fomdev/awaken/entries/AwakenPollinate.java
+++ src/main/java/com/fomdev/awaken/entries/AwakenPollinate.java
@@ -1,8 +1,13 @@
 package com.fomdev.awaken.entries;
 
 import com.fomdev.awaken.api.Registry;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.neoforged.neoforge.common.util.INBTSerializable;
+import org.jetbrains.annotations.NotNull;
 
 public abstract class AwakenPollinate extends Registry
 {
@@ -25,13 +30,55 @@ public abstract class AwakenPollinate extends Registry
             int level
     );
 
-    public Attribute getAttribute()
+    public final Attribute getAttribute()
     {
         return this.attribute;
     }
 
-    public EquipmentSlot[] getSlots()
+    public final EquipmentSlot[] getSlots()
     {
         return this.suitable;
+    }
+
+    public static class PollinateInstance implements INBTSerializable<CompoundTag>
+    {
+        private AwakenPollinate pollinate;
+        private int level;
+
+        public PollinateInstance(
+                AwakenPollinate pollinate,
+                int level
+        )
+        {
+            this.pollinate = pollinate;
+            this.level = level;
+        }
+
+        @Override
+        public CompoundTag serializeNBT(@NotNull HolderLookup.Provider provider)
+        {
+            CompoundTag self = new CompoundTag();
+            self.putString("id", pollinate.getLocation().toString());
+            self.putInt("level", level);
+
+            return self;
+        }
+
+        @Override
+        public void deserializeNBT(@NotNull HolderLookup.Provider provider, @NotNull CompoundTag tag)
+        {
+            this.pollinate = AwakenRegistries.AWAKEN_POLLINATE.getRegistry(ResourceLocation.parse(tag.getString("id")));
+            this.level = tag.getInt("level");
+        }
+
+        public AwakenPollinate getPollinate()
+        {
+            return this.pollinate;
+        }
+
+        public int getLevel()
+        {
+            return this.level;
+        }
     }
 }
--- src/main/java/com/fomdev/awaken/entries/AwakenSpore.java
+++ src/main/java/com/fomdev/awaken/entries/AwakenSpore.java
@@ -1,8 +1,13 @@
 package com.fomdev.awaken.entries;
 
 import com.fomdev.awaken.api.Registry;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.neoforged.neoforge.common.util.INBTSerializable;
+import org.jetbrains.annotations.NotNull;
 
 public abstract class AwakenSpore extends Registry
 {
@@ -25,13 +30,55 @@ public abstract class AwakenSpore extends Registry
             int level
     );
 
-    public Attribute getAttribute()
+    public final Attribute getAttribute()
     {
         return this.attribute;
     }
 
-    public EquipmentSlot[] getSlots()
+    public final EquipmentSlot[] getSlots()
     {
         return this.suitable;
+    }
+
+    public static class SporeInstance implements INBTSerializable<CompoundTag>
+    {
+        private AwakenSpore spore;
+        private int level;
+
+        public SporeInstance(
+                AwakenSpore spore,
+                int level
+        )
+        {
+            this.spore = spore;
+            this.level = level;
+        }
+
+        @Override
+        public CompoundTag serializeNBT(@NotNull HolderLookup.Provider provider)
+        {
+            CompoundTag self = new CompoundTag();
+            self.putString("id", spore.getLocation().toString());
+            self.putInt("level", level);
+
+            return self;
+        }
+
+        @Override
+        public void deserializeNBT(@NotNull HolderLookup.Provider provider, @NotNull CompoundTag tag)
+        {
+            this.spore = AwakenRegistries.AWAKEN_SPORE.getRegistry(ResourceLocation.parse(tag.getString("id")));
+            this.level = tag.getInt("level");
+        }
+
+        public AwakenSpore getSpore()
+        {
+            return this.spore;
+        }
+
+        public int getLevel()
+        {
+            return this.level;
+        }
     }
 }

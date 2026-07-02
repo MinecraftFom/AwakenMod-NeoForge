2aa35b5c4dad8036d032512423476b24afce38f8
MinecraftFom <MinecraftFom477@gmail.com>
2026-07-03 00:00:44 +0800

    Added a serializable instance for aspects

--- src/main/java/com/fomdev/awaken/entries/AwakenAspect.java
+++ src/main/java/com/fomdev/awaken/entries/AwakenAspect.java
@@ -1,6 +1,11 @@
 package com.fomdev.awaken.entries;
 
 import com.fomdev.awaken.api.Registry;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.resources.ResourceLocation;
+import net.neoforged.neoforge.common.util.INBTSerializable;
+import org.jetbrains.annotations.NotNull;
 
 public class AwakenAspect extends Registry
 {
@@ -9,9 +14,45 @@ public class AwakenAspect extends Registry
         super(id);
     }
 
-    public record AspectInstance(
-            AwakenAspect aspect,
-            int amount
-    )
-    {}
+    public static class AspectInstance implements INBTSerializable<CompoundTag>
+    {
+        private AwakenAspect aspect;
+        private int amount;
+
+        public AspectInstance(
+                AwakenAspect aspect,
+                int amount
+        )
+        {
+            this.aspect = aspect;
+            this.amount = amount;
+        }
+
+        @Override
+        public CompoundTag serializeNBT(@NotNull HolderLookup.Provider provider)
+        {
+            CompoundTag self = new CompoundTag();
+            self.putString("id", aspect.getLocation().toString());
+            self.putInt("level", amount);
+
+            return self;
+        }
+
+        @Override
+        public void deserializeNBT(@NotNull HolderLookup.Provider provider, @NotNull CompoundTag tag)
+        {
+            this.aspect = AwakenRegistries.AWAKEN_ASPECT.getRegistry(ResourceLocation.parse(tag.getString("id")));
+            this.amount = tag.getInt("level");
+        }
+
+        public AwakenAspect aspect()
+        {
+            return this.aspect;
+        }
+
+        public int amount()
+        {
+            return this.amount;
+        }
+    }
 }

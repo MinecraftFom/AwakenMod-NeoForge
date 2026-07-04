6df1a98382432c1d3677736d6988ac10384e3045
MinecraftFom <MinecraftFom477@gmail.com>
2026-07-04 18:16:42 +0800

    Added custom color render for itemstack tooltips if AwakenQuality is available

---
+++ src/main/java/com/fomdev/awaken/events/TooltipColorEvents.java
@@ -0,0 +1,34 @@
+package com.fomdev.awaken.events;
+
+import com.fomdev.awaken.entries.AwakenQuality;
+import com.fomdev.awaken.init.Awaken;
+import com.fomdev.awaken.util.ColorUtil;
+import com.fomdev.awaken.util.NBTUtil;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.client.event.RenderTooltipEvent;
+
+@EventBusSubscriber(modid = Awaken.MODID)
+public class TooltipColorEvents
+{
+    @SubscribeEvent
+    public static void onColor(
+            RenderTooltipEvent.Color event
+    )
+    {
+        ItemStack stack = event.getItemStack();
+        if (stack.is(Items.AIR))
+            return;
+
+        CompoundTag tag = NBTUtil.getModTag(stack);
+
+        AwakenQuality quality = NBTUtil.deserializeQuality(tag);
+        if (quality == null)
+            return;
+
+        ColorUtil.apply(event, ColorUtil.render(quality.getColors(), quality.getPattern()));
+    }
+}

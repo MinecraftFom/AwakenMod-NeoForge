8687c3c7c8bf90913f4520e1ef5cd021f94a37eb
MinecraftFom <MinecraftFom477@gmail.com>
2026-07-04 18:59:42 +0800

    Added Util.getStacks method to quick get all 'active' itemstacks fro mthe player

--- src/main/java/com/fomdev/awaken/util/Util.java
+++ src/main/java/com/fomdev/awaken/util/Util.java
@@ -1,7 +1,29 @@
 package com.fomdev.awaken.util;
 
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+
+import java.util.List;
+import java.util.stream.Stream;
+
 public class Util
 {
+    public static List<ItemStack> getStacks(
+            Player player
+    )
+    {
+        ItemStack main = player.getItemBySlot(EquipmentSlot.MAINHAND);
+        ItemStack off = player.getItemBySlot(EquipmentSlot.OFFHAND);
+        ItemStack helmet = player.getItemBySlot(EquipmentSlot.HEAD);
+        ItemStack chest = player.getItemBySlot(EquipmentSlot.CHEST);
+        ItemStack legs = player.getItemBySlot(EquipmentSlot.LEGS);
+        ItemStack feet = player.getItemBySlot(EquipmentSlot.FEET);
+
+        return Stream.of(main, off, helmet, chest, legs, feet).filter(s -> !s.is(Items.AIR)).toList();
+    }
+
     public static String format(
             String str,
             Object... args

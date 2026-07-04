9e778a3d27953fc554c7c6a5fe183e96a6cb9ba1
MinecraftFom <MinecraftFom477@gmail.com>
2026-07-04 18:15:18 +0800

    Changed the pollinate effects

--- src/main/java/com/fomdev/awaken/entries/AwakenPollinate.java
+++ src/main/java/com/fomdev/awaken/entries/AwakenPollinate.java
@@ -4,40 +4,56 @@ import com.fomdev.awaken.api.Registry;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.EquipmentSlot;
-import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.neoforged.neoforge.common.util.INBTSerializable;
 import org.jetbrains.annotations.NotNull;
 
 public abstract class AwakenPollinate extends Registry
 {
-    private final Attribute attribute;
-    private final EquipmentSlot[] suitable;
+    private final MobEffectInstance effect;
+    private final EquipmentSlot[] slots;
+    private final TriggerTarget target;
+    private final TriggerType type;
 
     public AwakenPollinate(
             String id,
-            Attribute attribute,
-            EquipmentSlot[] suitable
+            MobEffectInstance effect,
+            EquipmentSlot[] slots,
+            TriggerTarget target,
+            TriggerType type
     )
     {
         super(id);
 
-        this.attribute = attribute;
-        this.suitable = suitable;
+        this.effect = effect;
+        this.slots = slots;
+        this.target = target;
+        this.type = type;
     }
 
     public abstract double getAmount(
             int level
     );
 
-    public final Attribute getAttribute()
+    public final MobEffectInstance getEffect()
     {
-        return this.attribute;
+        return this.effect;
     }
 
     public final EquipmentSlot[] getSlots()
     {
-        return this.suitable;
+        return this.slots;
+    }
+
+    public TriggerTarget getTarget()
+    {
+        return this.target;
+    }
+
+    public TriggerType getType()
+    {
+        return this.type;
     }
 
     public static class PollinateInstance implements INBTSerializable<CompoundTag>
@@ -80,5 +96,19 @@ public abstract class AwakenPollinate extends Registry
         {
             return this.level;
         }
+    }
+
+    public enum TriggerTarget
+    {
+        DAMAGER,
+        SELF,
+        TARGET
+    }
+
+    public enum TriggerType
+    {
+        CONTINUE,
+        DAMAGE,
+        HURT
     }
 }

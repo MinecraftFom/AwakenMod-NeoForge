fbe6805afb20805ed926b8edb46375a6e7c7548c
MinecraftFom <MinecraftFom477@gmail.com>
2026-07-05 20:11:03 +0800

    Added a simple manager for enchantments. (Currently only features color)

---
+++ src/main/java/com/fomdev/awaken/enchant/EnchantManager.java
@@ -0,0 +1,36 @@
+package com.fomdev.awaken.enchant;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EnchantManager
+{
+    public static final List<Color> colors;
+
+    static
+    {
+        colors = List.of(
+                Color.BLACK,
+                Color.DARK_GRAY,
+                Color.GRAY,
+                Color.LIGHT_GRAY,
+                Color.WHITE,
+                new Color(0xBF1718),
+                new Color(0xE7C818),
+                Color.ORANGE,
+                Color.YELLOW,
+                new Color(0xE7FF00),
+                new Color(0x99E718),
+                new Color(0x49E718),
+                Color.GREEN,
+                new Color(0x00FF35),
+                new Color(0x00EFFF),
+                Color.CYAN,
+                Color.BLUE,
+                new Color(0x4100FF),
+                new Color(0x8A00FF),
+                Color.MAGENTA
+        );
+    }
+}

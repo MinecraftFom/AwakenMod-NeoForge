a4004c2ce125e105b2a69b1e6588cf8b8ecc0b0c
MinecraftFom <MinecraftFom477@gmail.com>
2026-07-07 23:19:07 +0800

    Registered stuff

--- src/main/java/com/fomdev/awaken/init/Awaken.java
+++ src/main/java/com/fomdev/awaken/init/Awaken.java
@@ -1,13 +1,20 @@
 package com.fomdev.awaken.init;
 
 import com.fomdev.awaken.entries.AwakenRegistries;
+import com.fomdev.awaken.register.AwakenAttachmentTypes;
+import com.fomdev.awaken.register.AwakenDataComponents;
+import com.fomdev.awaken.register.awaken.AwakenPollinates;
+import com.fomdev.awaken.register.awaken.AwakenQualities;
+import com.mojang.logging.LogUtils;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
+import org.slf4j.Logger;
 
 @Mod(Awaken.MODID)
 public class Awaken
 {
+    public static final Logger LOGGER = LogUtils.getLogger();
     public static final String MODID = "awaken";
 
     public Awaken(
@@ -16,5 +23,10 @@ public class Awaken
     )
     {
         AwakenRegistries.register(bus);
+        AwakenAttachmentTypes.register(bus);
+        AwakenDataComponents.register(bus);
+
+        AwakenPollinates.register();
+        AwakenQualities.register();
     }
 }

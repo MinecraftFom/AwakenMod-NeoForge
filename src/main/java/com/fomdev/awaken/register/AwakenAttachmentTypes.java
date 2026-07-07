ee532b36ef09252d0b30872a0ba319d8f9ddc07b
MinecraftFom <MinecraftFom477@gmail.com>
2026-07-07 23:17:26 +0800

    Added spawner attachments

---
+++ src/main/java/com/fomdev/awaken/register/AwakenAttachmentTypes.java
@@ -0,0 +1,40 @@
+package com.fomdev.awaken.register;
+
+import com.fomdev.awaken.init.Awaken;
+import com.mojang.serialization.Codec;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.attachment.AttachmentType;
+import net.neoforged.neoforge.registries.DeferredRegister;
+import net.neoforged.neoforge.registries.NeoForgeRegistries;
+
+import java.util.function.Supplier;
+
+public class AwakenAttachmentTypes
+{
+    public static final DeferredRegister<AttachmentType<?>> REGISTER =
+            DeferredRegister.create(
+                    NeoForgeRegistries.ATTACHMENT_TYPES,
+                    Awaken.MODID
+            );
+
+    public static final Supplier<AttachmentType<Integer>> SPAWNER_MAX_USE_ATTACHMENT =
+            REGISTER.register("spawner_max_use",
+                    () -> AttachmentType.builder(() -> 0)
+                            .serialize(Codec.INT)
+                            .build()
+            );
+
+    public static final Supplier<AttachmentType<Integer>> SPAWNER_USE_ATTACHMENT =
+            REGISTER.register("spawner_use",
+                    () -> AttachmentType.builder(() -> 0)
+                            .serialize(Codec.INT)
+                            .build()
+            );
+
+    public static void register(
+            IEventBus bus
+    )
+    {
+        REGISTER.register(bus);
+    }
+}

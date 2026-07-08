8b20b6db24189a238066e0c8d2e2c484398fac7d
MinecraftFom <MinecraftFom477@gmail.com>
2026-07-08 18:13:44 +0800

    Added synced spawner data and awaken level data (attachment)

--- src/main/java/com/fomdev/awaken/register/data/AwakenAttachmentTypes.java
+++ src/main/java/com/fomdev/awaken/register/data/AwakenAttachmentTypes.java
@@ -1,7 +1,12 @@
 package com.fomdev.awaken.register.data;
 
 import com.fomdev.awaken.init.Awaken;
+import com.fomdev.awaken.util.Records;
 import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import io.netty.buffer.ByteBuf;
+import net.minecraft.network.codec.ByteBufCodecs;
+import net.minecraft.network.codec.StreamCodec;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.attachment.AttachmentType;
 import net.neoforged.neoforge.registries.DeferredRegister;
@@ -11,16 +16,47 @@ import java.util.function.Supplier;
 
 public class AwakenAttachmentTypes
 {
+    public static final Codec<Records.AwakenLevelComponent> AWAKEN_LEVEL_CODEC =
+            RecordCodecBuilder.create(
+                    inst ->
+                            inst
+                                    .group(
+                                            Codec.FLOAT
+                                                    .fieldOf("awaken_level")
+                                                    .forGetter(Records.AwakenLevelComponent::level)
+                                    )
+                                    .apply(
+                                            inst,
+                                            Records.AwakenLevelComponent::new
+                                    )
+            );
+
+    public static final StreamCodec<ByteBuf, Records.AwakenLevelComponent> AWAKEN_LEVEL_STREAM_CODEC =
+            StreamCodec.composite(
+                    ByteBufCodecs.FLOAT,
+                    Records.AwakenLevelComponent::level,
+                    Records.AwakenLevelComponent::new
+            );
+
     public static final DeferredRegister<AttachmentType<?>> REGISTER =
             DeferredRegister.create(
                     NeoForgeRegistries.ATTACHMENT_TYPES,
                     Awaken.MODID
             );
 
+    public static final Supplier<AttachmentType<Records.AwakenLevelComponent>> PLAYER_AWAKEN_LEVEL_ATTACHMENT =
+            REGISTER.register("awaken_level",
+                    () -> AttachmentType.builder(() -> new Records.AwakenLevelComponent(0.0F))
+                            .serialize(AWAKEN_LEVEL_CODEC)
+                            .sync((holder, to) -> holder == to, AWAKEN_LEVEL_STREAM_CODEC)
+                            .build()
+            );
+
     public static final Supplier<AttachmentType<Integer>> SPAWNER_MAX_USE_ATTACHMENT =
             REGISTER.register("spawner_max_use",
                     () -> AttachmentType.builder(() -> 0)
                             .serialize(Codec.INT)
+                            .sync((holder, to) -> true, ByteBufCodecs.INT)
                             .build()
             );
 
@@ -28,6 +64,7 @@ public class AwakenAttachmentTypes
             REGISTER.register("spawner_use",
                     () -> AttachmentType.builder(() -> 0)
                             .serialize(Codec.INT)
+                            .sync((holder, to) -> true, ByteBufCodecs.INT)
                             .build()
             );
 

6a74e7af20a7aba994c8cbd34d780549aab132f6
MinecraftFom <MinecraftFom477@gmail.com>
2026-07-08 23:54:22 +0800

    Moved some translation method to it

--- src/main/java/com/fomdev/awaken/util/LocaleUtil.java
+++ src/main/java/com/fomdev/awaken/util/LocaleUtil.java
@@ -2,6 +2,11 @@ package com.fomdev.awaken.util;
 
 import com.fomdev.awaken.entries.*;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LocaleUtil
 {
@@ -19,6 +24,17 @@ public class LocaleUtil
         return Component.translatable("infix." + infix.id() + ".name");
     }
 
+    public static String localizeOperation(
+            AttributeModifier.Operation operation
+    )
+    {
+        return switch (operation) {
+            case ADD_VALUE -> "+ ";
+            case ADD_MULTIPLIED_BASE -> "*= ";
+            case ADD_MULTIPLIED_TOTAL -> "* ";
+        };
+    }
+
     public static Component localizePrefix(
             AwakenPrefix prefix
     )
@@ -33,10 +49,35 @@ public class LocaleUtil
         return Component.translatable("quality." + quality.id() + ".name");
     }
 
+    public static List<Component> localizeSlots(
+            EquipmentSlot[] slots
+    )
+    {
+        List<Component> components = new ArrayList<>();
+        for (EquipmentSlot slot: slots)
+            components.add(Component.empty().append(Component.translatable("tooltip.slot.avaiable.info")).append(Component.translatable(slot.getName())));
+
+        return components;
+    }
+
     public static Component localizeSuffix(
             AwakenSuffix suffix
     )
     {
         return Component.translatable("suffix." + suffix.id() + ".name");
+    }
+
+    public static Component localizeTarget(
+            AwakenPollinate.TriggerTarget target
+    )
+    {
+        return Component.translatable(target.getUnlocalized());
+    }
+
+    public static Component localizeTrigger(
+            AwakenPollinate.TriggerType type
+    )
+    {
+        return Component.translatable(type.getUnlocalized());
     }
 }

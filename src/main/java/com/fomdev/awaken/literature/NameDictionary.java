65ff00721ac8899978fea846e85593632eea115c
Fom477 <MinecraftFom477@gmail.com>
2026-08-06 23:58:43 +0800

    Added a total weight for faster calculation (since the weight of each NameEntry is static)

--- src/main/java/com/fomdev/awaken/literature/NameDictionary.java
+++ src/main/java/com/fomdev/awaken/literature/NameDictionary.java
@@ -1,35 +1,35 @@
 package com.fomdev.awaken.literature;
 
+import com.fomdev.awaken.init.config.AwakenLiterature;
 import net.minecraft.network.chat.Component;
 import net.minecraft.util.RandomSource;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.AbstractList;
 import java.util.List;
 
 public class NameDictionary extends AbstractList<NameDictionary.NameEntry> implements List<NameDictionary.NameEntry>
 {
+    public int weight;
+
     private final int size;
     private final NameEntry[] nameEntries;
 
-    public NameDictionary(
-    )
+    public NameDictionary()
     {
-        // TODO: add find logic
-        this.nameEntries = new NameEntry[]{};
+        this.nameEntries = loadFromConfig();
         this.size = this.nameEntries.length;
+
+        for (NameEntry entry: this)
+            weight += entry.weight;
     }
 
     public Component get(
             RandomSource random
     )
     {
-        int totalWeight = 0;
         int currentIndex = 0;
-
-        for (NameEntry entry: this)
-            totalWeight += entry.weight;
-
-        totalWeight = random.nextInt(totalWeight);
+        int totalWeight = random.nextInt(weight);
 
         while (totalWeight >= 0)
         {
@@ -52,6 +52,41 @@ public class NameDictionary extends AbstractList<NameDictionary.NameEntry> imple
         return this.size;
     }
 
-    public record NameEntry(Component value, int weight) {
+    public record NameEntry(Component value, int weight)
+    {
+        @Override
+        public @NotNull String toString()
+        {
+            return value + "|" + weight;
+        }
+    }
+
+    public static NameEntry[] loadFromConfig()
+    {
+        return AwakenLiterature.CONFIG.NAMES.get()
+                .stream()
+                .map(
+                        NameDictionary::loadFromString
+                )
+                .toArray(
+                        NameEntry[]::new
+                );
+    }
+
+    public static NameEntry loadFromString(
+            String raw
+    )
+    {
+        String[] components = raw.strip().split("\\|");
+        if (components.length != 2)
+            throw new IllegalArgumentException("Illegal argument structure, required 2 parts");
+
+        String header = components[0];
+        String body = components[1];
+
+        Component title = Component.literal(header);
+        Integer chance = Integer.getInteger(body);
+
+        return new NameEntry(title, chance);
     }
 }

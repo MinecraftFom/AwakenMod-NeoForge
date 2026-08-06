c12d571ceb14a990abf6bc41ecc89101906dd01c
Fom477 <MinecraftFom477@gmail.com>
2026-08-06 23:59:55 +0800

    Added a literature config file

---
+++ src/main/java/com/fomdev/awaken/init/config/AwakenLiterature.java
@@ -0,0 +1,34 @@
+package com.fomdev.awaken.init.config;
+
+import net.neoforged.neoforge.common.ModConfigSpec;
+import org.apache.commons.lang3.tuple.Pair;
+
+import java.util.List;
+
+public class AwakenLiterature
+{
+    public static final AwakenLiterature CONFIG;
+    public static final ModConfigSpec SPEC;
+
+    public final ModConfigSpec.ConfigValue<List<String>> NAMES;
+
+    public AwakenLiterature(
+            ModConfigSpec.Builder builder
+    )
+    {
+        builder.push("literature");
+
+        this.NAMES = builder
+                .comment("Names for generating mobs, structure: [NAME]|[CHANCE]")
+                .define("name_entries", List.of());
+    }
+
+    static
+    {
+        Pair<AwakenLiterature, ModConfigSpec> pair =
+                new ModConfigSpec.Builder().configure(AwakenLiterature::new);
+
+        CONFIG = pair.getLeft();
+        SPEC = pair.getRight();
+    }
+}

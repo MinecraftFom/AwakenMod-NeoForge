33d57133640070d641ea665c71f53dfdae4976e2
Fom477 <MinecraftFom477@gmail.com>
2026-08-17 18:05:57 +0800

    Bug fix

--- src/main/java/com/fomdev/awaken/mixin/MixinTrialSpawnerConfig.java
+++ src/main/java/com/fomdev/awaken/mixin/MixinTrialSpawnerConfig.java
@@ -2,6 +2,7 @@ package com.fomdev.awaken.mixin;
 
 import com.fomdev.awaken.difficulty.DifficultyManager;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.server.IntegratedServer;
 import net.minecraft.world.level.block.entity.trialspawner.TrialSpawnerConfig;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -17,7 +18,15 @@ public class MixinTrialSpawnerConfig
             CallbackInfoReturnable<Integer> cir
     )
     {
-        cir.setReturnValue((int) DifficultyManager.getLevelDifficulty(Minecraft.getInstance().getSingleplayerServer().overworld()) * cir.getReturnValue());
+        IntegratedServer server = Minecraft.getInstance().getSingleplayerServer();
+        if (server == null)
+            return;
+
+        float diff = DifficultyManager.getLevelDifficulty(server.overworld());
+        float factor = (float) Math.pow(diff, 1.0 / 10.0);
+        float factor2 = factor <= 0? 1: factor;
+
+        cir.setReturnValue((int) (factor2 * cir.getReturnValue()));
     }
 
     @Inject(method = "calculateTargetSimultaneousMobs", at = @At("RETURN"), cancellable = true)
@@ -26,6 +35,14 @@ public class MixinTrialSpawnerConfig
             CallbackInfoReturnable<Integer> cir
     )
     {
-        cir.setReturnValue((int) DifficultyManager.getLevelDifficulty(Minecraft.getInstance().getSingleplayerServer().overworld()) * cir.getReturnValue());
+        IntegratedServer server = Minecraft.getInstance().getSingleplayerServer();
+        if (server == null)
+            return;
+
+        float diff = DifficultyManager.getLevelDifficulty(server.overworld());
+        float factor = (float) Math.pow(diff, 1.0 / 10.0);
+        float factor2 = factor <= 0? 1: factor;
+
+        cir.setReturnValue((int) (factor2 * cir.getReturnValue()));
     }
 }
